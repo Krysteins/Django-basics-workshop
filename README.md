56145e185abdb333ad7c40a6855203029e9e7179
Krysteins <krysiak.mateusz.94@gmail.com>
2022-05-08 14:04:24 +0200

    view add room

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ Add a model to represent the room. It stores information such as:
 * room capacity (integer numeric type field),
 * availability of the projector (field type `boolean`).
 
+# Workshop &ndash; The view of adding a room
+
+Create a view that allows to add a new room. Place it at `/ room / new /`.
+The view:
+* after entering the ** GET ** method, display a form with the following fields:
+     * name of the room &ndash; text,
+     * room capacity &ndash; number,
+     * projector availability &ndash; checkbox.
+* after entering using the ** POST ** method:
+     * if the name of the room is not empty,
+     * if the room with the given name no longer exists in the database,
+     * if the capacity of the room is a positive number;
+     * if the data is correct, save the new room to the database and redirect the user to the home page,
+     * if they are incorrect, it should display an appropriate message to the user.
--- reservation/reservation_app/views.py
+++ reservation/reservation_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import ConferenceRoom
 
-# Create your views here.
+
+class AddRoomView(View):
+    def get(self, request):
+        return render(request, "add_room.html")
+
+    def post(self, request):
+        name = request.POST.get("room-name")
+        capacity = request.POST.get("capacity")
+        capacity = int(capacity) if capacity else 0
+        projector = request.POST.get("projector") == "on"
+
+        if not name:
+            return render(request, "add_room.html", context={"error": "The name of the room was not given"})
+        if capacity <= 0:
+            return render(request, "add_room.html", context={"error": "The capacity of the room must be positive"})
+        if ConferenceRoom.objects.filter(name=name).first():
+            return render(request, "add_room.html", context={"error": "A room with the given name exists"})
+
+        ConferenceRoom.objects.create(name=name, capacity=capacity, projector_availability=projector)
+        return redirect("room-list")
+
